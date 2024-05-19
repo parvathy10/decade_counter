3c78ec7fc13cc0609505ccddfa5d45ca582bcbb8
Parvathy Harikumar <parvathyharikumar01@gmail.com>
2024-05-19 18:57:41 +0530

    odified

--- SimpleCounter.v
+++ SimpleCounter.v
@@ -4,13 +4,13 @@
 module SimpleCounter(
   input wire clk,
   input wire rst,
-  output reg [3:0] count
+  output reg [1:0] count
 );
 
   always @(posedge clk or posedge rst) begin
     if (rst) begin
-      // Reset the counter to 4'b0000 when reset is asserted
-      count <= 4'b0000;
+      // Reset the counter to 2b00 when reset is asserted
+      count <= 2'b00;
     end else begin
       // Increment the counter on each rising edge of the clock
       count <= count + 1;
